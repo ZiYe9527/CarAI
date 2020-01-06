490ffb5fc0051e7fe9181c9d0a22e5e00f81c2f1
Setiadi Pratama <idaites23@gmail.com>
2020-01-06 22:24:02 +0700

    Part4

--- Assets/Scripts/CarEngine.cs
+++ Assets/Scripts/CarEngine.cs
@@ -1,22 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CarEngine : MonoBehaviour {
 
-    public float force = 1;
+    public Transform path;
+    public float maxSteerAngle = 45f;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public float maxMotorTorque = 80f;
+    public float currentSpeed;
+    public float maxSpeed = 100f;
 
-    private Rigidbody rb;
+    private List<Transform> nodes;
+    private int currentNode = 0;
+    
 
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
+        void Start () {
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != path.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+    }
 	
-	void Update () {
-        if (Input.GetKey(KeyCode.W)) {
-            rb.AddForce(transform.forward * force);
+    private void FixedUpdate()
+    {
+        ApplySteer();
+        Drive();
+        CheckWaypointDistance();
+    }
+	
+    private void ApplySteer()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        wheelFL.steerAngle = newSteer;
+        wheelFR.steerAngle = newSteer;
+    }
+
+    private void Drive()
+    {
+        currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 100;
+        if(currentSpeed < maxSpeed)
+        {
+            wheelFR.motorTorque = maxMotorTorque;
+            wheelFL.motorTorque = maxMotorTorque;
         }
-        if (Input.GetKey(KeyCode.A)) {
-            rb.AddTorque(transform.up * -force);
+        else
+        {
+            wheelFR.motorTorque = 0;
+            wheelFL.motorTorque = 0;
         }
-	}
+        
+    }
+
+    private void CheckWaypointDistance()
+    {
+        if(Vector3.Distance(transform.position, nodes[currentNode].position) < 0.5f){
+            if(currentNode == nodes.Count - 1)
+            {
+                currentNode = 0;
+            }
+            else
+            {
+                currentNode++; 
+            }
+        }
+    }
 }
